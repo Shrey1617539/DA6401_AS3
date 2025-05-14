a95702f157aef61b70916874d85e734e7c1d23c7
Shrey1617539 <95377951+Shrey1617539@users.noreply.github.com>
2025-05-15 00:32:06 +0530

    infrence added and model building fixed

--- helping_functions.py
+++ helping_functions.py
@@ -28,7 +28,7 @@ class seq2seq:
         self.encoder_type = encoder_type
         self.decoder_type = decoder_type
     
-    def build_model(self):
+    def build_training_model(self):
         encoder_inputs = keras.layers.Input(shape=(None,), name='encoder_inputs')
         encoder_embedding = keras.layers.Embedding(
             input_dim=self.input_vocab_size,
@@ -42,7 +42,7 @@ class seq2seq:
 
         for i in range(self.encoder_layers):
             return_sequences = (i < self.encoder_layers - 1)
-            return_state = (i == self.encoder_layers - 1)
+            return_state = True
 
             if self.encoder_type == 'LSTM':
                 rnn_layer = keras.layers.LSTM(
@@ -54,11 +54,8 @@ class seq2seq:
                     name=f'encoder_{i}'
                 )
 
-                if return_state:
-                    encoder_outputs, state_h, state_c = rnn_layer(encoder_outputs)
-                    encoder_states = [state_h, state_c]
-                else:
-                    encoder_outputs = rnn_layer(encoder_outputs)
+                encoder_outputs, state_h, state_c = rnn_layer(encoder_outputs)
+                encoder_states.extend([state_h, state_c])
             
             elif self.encoder_type == 'GRU':
                 rnn_layer = keras.layers.GRU(
@@ -70,12 +67,9 @@ class seq2seq:
                     name=f'encoder_{i}'
                 )
 
-                if return_state:
-                    encoder_outputs, state_h = rnn_layer(encoder_outputs)
-                    encoder_states = [state_h]
-                else:
-                    encoder_outputs = rnn_layer(encoder_outputs)
-            
+                encoder_outputs, state_h = rnn_layer(encoder_outputs)
+                encoder_states.append(state_h)
+
             elif self.encoder_type == 'RNN':
                 rnn_layer = keras.layers.SimpleRNN(
                     units=self.hidden_units,
@@ -86,11 +80,8 @@ class seq2seq:
                     name=f'encoder_{i}'
                 )
 
-                if return_state:
-                    encoder_outputs, state_h = rnn_layer(encoder_outputs)
-                    encoder_states = [state_h]
-                else:
-                    encoder_outputs = rnn_layer(encoder_outputs)
+                encoder_outputs, state_h = rnn_layer(encoder_outputs)
+                encoder_states.append(state_h)
         
         decoder_inputs = keras.layers.Input(shape=(None,), name='decoder_inputs')
         decoder_embedding = keras.layers.Embedding(
@@ -101,11 +92,33 @@ class seq2seq:
         )(decoder_inputs)
 
         decoder_outputs = decoder_embedding
-        decoder_states = encoder_states
+        decoder_init_states = []
+        idx = 0
+        for i in range(self.decoder_layers):
+            if i<self.encoder_layers:
+                if self.decoder_type == 'LSTM':
+                    h = encoder_states[idx]
+                    c = encoder_states[idx + 1]
+                    decoder_init_states.append([h,c])
+                    idx += 2
+                else:
+                    h = encoder_states[idx]
+                    decoder_init_states.append([h])
+                    idx += 1
+            else:
+                if self.decoder_type == 'LSTM':
+                    h = encoder_states[-2]
+                    c = encoder_states[-1]
+                    decoder_init_states.append([h,c])
+                else:
+                    h = encoder_states[-1]
+                    decoder_init_states.append([h])
+
+                
 
         for i in range(self.decoder_layers):
             return_sequences = True
-            return_state = False
+            return_state = True
 
             if self.decoder_type == 'LSTM':
                 rnn_layer = keras.layers.LSTM(
@@ -116,10 +129,7 @@ class seq2seq:
                     recurrent_dropout=self.recurrent_dropout_rate,
                     name=f'decoder_{i}'
                 )
-                if i == 0:
-                    decoder_outputs = rnn_layer(decoder_outputs, initial_state=decoder_states)
-                else:
-                    decoder_outputs = rnn_layer(decoder_outputs)
+                decoder_outputs, _, _ = rnn_layer(decoder_outputs, initial_state=decoder_init_states[i])
                 
             elif self.decoder_type == 'GRU':
                 rnn_layer = keras.layers.GRU(
@@ -130,11 +140,8 @@ class seq2seq:
                     recurrent_dropout=self.recurrent_dropout_rate,
                     name=f'decoder_{i}'
                 )
+                decoder_outputs, _ = rnn_layer(decoder_outputs, initial_state=decoder_init_states[i])
 
-                if i == 0:
-                    decoder_outputs = rnn_layer(decoder_outputs, initial_state=decoder_states)
-                else:
-                    decoder_outputs = rnn_layer(decoder_outputs)
             
             elif self.decoder_type == 'RNN':
                 rnn_layer = keras.layers.SimpleRNN(
@@ -145,11 +152,8 @@ class seq2seq:
                     recurrent_dropout=self.recurrent_dropout_rate,
                     name=f'decoder_{i}'
                 )
+                decoder_outputs, _ = rnn_layer(decoder_outputs, initial_state=decoder_init_states[i])
 
-                if i == 0:
-                    decoder_outputs = rnn_layer(decoder_outputs, initial_state=decoder_states)
-                else:
-                    decoder_outputs = rnn_layer(decoder_outputs)
 
         decoder_dense = keras.layers.Dense(
             units=self.output_vocab_size,
@@ -160,4 +164,149 @@ class seq2seq:
         self.training_model = keras.models.Model(
             inputs=[encoder_inputs, decoder_inputs],
             outputs=decoder_outputs
-        )                
+        )
+    
+    def build_inference_model(self):
+        encoder_inputs = self.training_model.get_layer('encoder_inputs').input
+        encoder_embedding = self.training_model.get_layer('encoder_embedding')(encoder_inputs)
+
+        encoder_output = encoder_embedding
+        encoder_states = []
+        for i in range(self.encoder_layers):
+            layer = self.training_model.get_layer('encoder_{}'.format(i))
+            encoder_output, *states = layer(encoder_output)
+            encoder_states.extend(states)
+
+
+        # if self.encoder_type == 'LSTM':
+        #     encoder_states = [
+        #         self.training_model.get_layer('encoder_{}'.format(self.encoder_layers - 1)).output[0],
+        #         self.training_model.get_layer('encoder_{}'.format(self.encoder_layers - 1)).output[1]
+        #     ]
+        # else:
+        #     encoder_states = [
+        #         self.training_model.get_layer('encoder_{}'.format(self.encoder_layers - 1)).output
+        #     ]
+        
+        self.encoder_model = keras.models.Model(
+            inputs=encoder_inputs,
+            outputs=encoder_states
+        )
+
+
+
+        decoder_inputs = self.training_model.get_layer('decoder_inputs').input
+        decoder_embedding = self.training_model.get_layer('decoder_embedding')(decoder_inputs)
+        
+        decoder_states_inputs = []
+        for idx, state in enumerate(encoder_states):
+            decoder_states_inputs.append(
+                keras.layers.Input(shape=(self.hidden_units,), name=f'decoder_state_input_{idx}')
+            )
+        # if self.decoder_type == 'LSTM':
+        #     decoder_states_inputs = keras.layers.Input(shape=(self.hidden_units,))
+        # else:
+        #     decoder_state_input = keras.layers.Input(shape=(self.hidden_units,))
+        #     decoder_states_inputs = [decoder_state_input]
+        
+
+        decoder_outputs = decoder_embedding
+        decoder_states = []
+        state_idx = 0
+        for i in range(self.decoder_layers):
+            layer = self.training_model.get_layer('decoder_{}'.format(i))
+            if i < self.encoder_layers:
+                if self.decoder_type == 'LSTM':
+                    init_h = decoder_states_inputs[state_idx]
+                    init_c = decoder_states_inputs[state_idx + 1]
+                    decoder_outputs, state_h, state_c = layer(
+                        decoder_outputs,
+                        initial_state=[init_h, init_c]
+                    )
+                    decoder_states.extend([state_h, state_c])
+                    state_idx += 2
+                else:
+                    init_h = decoder_states_inputs[state_idx]
+                    decoder_outputs, state_h = layer(
+                        decoder_outputs,
+                        initial_state=[init_h]
+                    )
+                    decoder_states.append(state_h)
+                    state_idx += 1
+            else:
+                if self.decoder_type == 'LSTM':
+                    init_h = decoder_states_inputs[-2]
+                    init_c = decoder_states_inputs[-1]
+                    decoder_outputs, state_h, state_c = layer(
+                        decoder_outputs,
+                        initial_state=[init_h, init_c]
+                    )
+                    decoder_states.extend([state_h, state_c])
+                else:
+                    init_h = decoder_states_inputs[-1]
+                    decoder_outputs, state_h = layer(
+                        decoder_outputs,
+                        initial_state=[init_h]
+                    )
+                    decoder_states.append(state_h)
+
+            # outputs = layer(decoder_outputs, initial_state=decoder_states_inputs)
+    
+            
+            # if self.decoder_type == 'LSTM':
+            #     decoder_outputs, state_h, state_c = outputs
+            #     decoder_states = [state_h, state_c]
+            # else:
+            #     decoder_outputs, state_h = outputs
+                # decoder_states = [state_h]
+        
+        decoder_outputs = self.training_model.get_layer('decoder_dense')(decoder_outputs)
+
+        self.decoder_model = keras.models.Model(
+            inputs=[decoder_inputs] + decoder_states_inputs,
+            outputs=[decoder_outputs] + decoder_states
+        )
+        # for i in range(self.decoder_layers):
+        #     if self.decoder_type == 'LSTM':
+        #         decoder_lstm = self.training_model.get_layer('decoder_{}'.format(i))
+
+        #         if i == 0:
+        #             decoder_outputs, state_h, state_c = decoder_lstm(
+        #                 decoder_outputs,
+        #                 initial_state=decoder_states_inputs
+        #             )
+        #             decoder_states = [state_h, state_c]
+        #         else:
+        #             decoder_outputs = decoder_lstm(decoder_outputs)
+            
+        #     elif self.decoder_type == 'GRU':
+        #         decoder_gru = self.training_model.get_layer('decoder_{}'.format(i))
+
+        #         if i == 0:
+        #             decoder_outputs, state_h = decoder_gru(
+        #                 decoder_outputs,
+        #                 initial_state=decoder_states_inputs
+        #             )
+        #             decoder_states = [state_h]
+        #         else:
+        #             decoder_outputs = decoder_gru(decoder_outputs)
+            
+        #     elif self.decoder_type == 'RNN':
+        #         decoder_rnn = self.training_model.get_layer('decoder_{}'.format(i))
+
+        #         if i == 0:
+        #             decoder_outputs, state_h = decoder_rnn(
+        #                 decoder_outputs,
+        #                 initial_state=decoder_states_inputs
+        #             )
+        #             decoder_states = [state_h]
+        #         else:
+        #             decoder_outputs = decoder_rnn(decoder_outputs)
+        
+        # decoder_dense = self.training_model.get_layer('decoder_dense')
+        # decoder_outputs = decoder_dense(decoder_outputs)
+        # self.decoder_model = keras.models.Model(
+        #     inputs=[decoder_inputs] + decoder_states_inputs,
+        #     outputs=[decoder_outputs] + decoder_states
+        # )
+                    
